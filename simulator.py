eee82a59c11e7dd34617b9cf046a46e2b7e0f823
Ryzhevnin Maksim <maxrizhalex@mail.ru>
2022-07-01 13:33:11 +0300

    small change in simulator.py

--- simulator.py
+++ simulator.py
@@ -8,17 +8,18 @@ from utils.window import Window
 
 g = Window("Test, don't touch!!!", cfg.width, cfg.height, cfg.fps)
 
-num = 10
+num = 2
 
 segs = []
 for i in range(num):
-    segs.append(Segment(360 + i*10, 360, 10, 10, np.pi * (90/180), 10))
+    segs.append(Segment(360 - i*40, 360, 40, 10, np.pi * (90/180), 1))
 '''
 a = Segment(360, 360, 100, 20, np.pi * (90/180), 1000)
 b = Segment(360, 360+50, 100, 20, np.pi * (0/180), 1000)
 c = Segment(360, 360, 100, 20, np.pi * (-90/180), 1000)
 '''
-r = Rope(segs, ph = 1)
+r = Rope(segs, ph=Rope.PHYSICS_NEWTON)
+
 g.objects.append(r)
 
 
